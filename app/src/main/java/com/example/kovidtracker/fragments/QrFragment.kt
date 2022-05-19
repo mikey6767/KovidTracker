169d286655e4fe3c3d7c4b25f59ec5e691441df5
kwei1919 <kwei1919@gmail.com>
2022-05-19 14:57:51 +0800

    Wei

--- app/src/main/java/com/example/kovidtracker/fragments/QrFragment.kt
+++ app/src/main/java/com/example/kovidtracker/fragments/QrFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import com.example.kovidtracker.R
@@ -12,6 +13,7 @@ import com.example.kovidtracker.R
 class QrFragment : Fragment() {
 
     lateinit var qr: TextView
+    lateinit var btn_qr: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +26,18 @@ class QrFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        qr = view.findViewById<TextView>(R.id.tv_qr)
+        qr = view.findViewById(R.id.tv_qr)
+        btn_qr = view.findViewById(R.id.btn_qr)
 
         qr.setOnClickListener{
             Toast.makeText(context, "This is QR Fragment", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_qr.setOnClickListener {
+            val newFragment = CheckInResultFragment()
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragment_container, newFragment)
+            transaction?.commit()
         }
     }
 
