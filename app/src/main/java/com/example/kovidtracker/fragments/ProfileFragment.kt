31a906f63acee5bb9c529e599cf85237c7c0c21d
swe1902119 <65288733+swe1902119@users.noreply.github.com>
2022-05-26 14:04:26 +0800

    Profile button fixed

--- app/src/main/java/com/example/kovidtracker/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/kovidtracker/fragments/ProfileFragment.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.kovidtracker.R
 
 class ProfileFragment : Fragment() {
 
+    private var usrname = ""
     //lateinit var profile: TextView
+    //private val buttonsFragMap = mapOf(R.id.profile_his_btn to HistoryFragment.newInstance(usrname), R.id.profile_hs_btn to HealthStatusFragment.newInstance(usrname), R.id.profile_faq to FaqFragment.newInstance(usrname))
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
@@ -22,12 +22,28 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<Button>(R.id.profile_his_btn).setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HistoryFragment.newInstance("AAA")).commit()
+        }
+        view.findViewById<Button>(R.id.profile_hs_btn).setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HealthStatusFragment.newInstance("AAA")).commit()
+        }
+        view.findViewById<Button>(R.id.profile_faq).setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FaqFragment.newInstance("AAA")).commit()
+        }
         //profile = view.findViewById<TextView>(R.id.tv_profile)
 
         /*profile.setOnClickListener{
             Toast.makeText(context, "This is Profile Fragment", Toast.LENGTH_SHORT).show()
         }*/
         //requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HistoryFragment.newInstance("AAA")).commit()
-        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HealthStatusFragment.newInstance("AAA")).commit()
+        //requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HealthStatusFragment.newInstance("AAA")).commit()
     }
+
+    /*override fun onClick(btn: View?) {
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_container, buttonsFragMap[btn?.id]!!)
+    }*/
 }
